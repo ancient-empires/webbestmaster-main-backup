effdcffd30bc884bc764d644283d2833166de850
webbestmaster <web.best.master@gmail.com>
2015-01-28 20:39:44 +0300

    AOE - fix lang.

--- AncientOfEmpires/www/js/lang/en.js
+++ AncientOfEmpires/www/js/lang/en.js
@@ -12,47 +12,47 @@
 		thanks: 'Programmer:<br>Dmitry Turovtsov<br><br>Thanks:<br>Pavel Prylutski<br>Igor Kupreev',
 		units: {
 			archer: {
-				name: 'en archer',
+				name: 'archer',
 				description: 'With their powerful bows archers can attack from a distance and are especially powerful against airborne enemies.'
 			},
 			bones: {
-				name: 'en bones',
+				name: 'bones',
 				description: 'Summoned by Wizards, these lifeless warriors are as strong as soldiers and deadly opponents on any battlefield.'
 			},
 			catapult: {
-				name: 'en catapult',
+				name: 'catapult',
 				description: 'Catapults bring devastation wherever they go with their enormous attack range. However, their relative immobility and inability to attack up close make them vulnerable so guard them well. Catapults can either move or attack during a turn, but not do both.'
 			},
 			golem: {
-				name: 'en golem',
+				name: 'golem',
 				description: 'Forged out of living rock by the dwarf tribes of the Lost Mountains, Golems are slow but immensely strong in defence. A golem stationed in a building or a well defended mountain is very difficult to defeat.'
 			},
 			knight: {
-				name: 'en knight',
+				name: 'knight',
 				description: 'The Knight is very strong in attack and defence. Knight can also occupy farms and castles to produce troops and can be revived in the castle if they fall in battle.'
 			},
 			lizard: {
-				name: 'en lizard',
+				name: 'lizard',
 				description: 'Descendants of the ancient amphibian empire spanning the marshlands in the east Lizards are proud warriors. They are immensely mobile (+3) and stronger in defence (+2) when in water.'
 			},
 			soldier: {
-				name: 'en soldier',
+				name: 'soldier',
 				description: 'Hardworking and brave, soldiers form the backbone of any army. They are also the only unit that can capture farms to earn gold.'
 			},
 			spider: {
-				name: 'en spider',
+				name: 'spider',
 				description: 'Spiders are dangerous creatures whose natural habitat spans the unforgiving dark forests of the west. In addition to being highly mobile, spiders use a poisoned attack to weaken their opponents for one turn.'
 			},
 			wisp: {
-				name: 'en wisp',
+				name: 'wisp',
 				description: 'These mystical beings of pure light radiate an aura which adds attack points to nearby friendly units. In close combat they are especially deadly against skeletons.'
 			},
 			wizard: {
-				name: 'en wizard',
+				name: 'wizard',
 				description: 'The ancient guild of wizards are weak at close combat, but immensely useful as they can summon fighting skeletons from dead troops.'
 			},
 			wyvern: {
-				name: 'en wyvern',
+				name: 'wyvern',
 				description: 'These majestic flying beasts have ruled over the mountains of mist since ancient times. They are extremely mobile as well as deadly in attack on land, air and sea.'
 			}
 		},
