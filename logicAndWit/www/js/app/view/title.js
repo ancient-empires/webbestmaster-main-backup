838838e792aff905671172ed116becee5ea009f8
webbestmaster <web.best.master@gmail.com>
2015-02-04 01:16:09 +0300

    LW - IP.

--- logicAndWit/www/js/app/view/title.js
+++ logicAndWit/www/js/app/view/title.js
@@ -15,11 +15,23 @@
 
 		initialize: function () {
 
-			this.$el = $(this.tmpl.title());
+			this.$el = $(this.tmpl.title({ getProgressBySection: this.getProgressBySection }));
 
 			this.proto.initialize.apply(this, arguments);
 
 			this.render();
+
+		},
+
+		getProgressBySection: function (jsPath, section) {
+
+			var questionLength = section.questions.length,
+				info = win.APP.info,
+				sections = info.get('sections') || {},
+				questionsDone = sections[jsPath] || {},
+				questionsDoneLength = _.keys(questionsDone).length;
+
+			return questionsDoneLength / questionLength * 100;
 
 		}
 
