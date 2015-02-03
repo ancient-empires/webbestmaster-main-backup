f87d4aab3f3114a87c37052007c1f75cf8ab33df
webbestmaster <web.best.master@gmail.com>
2015-02-03 23:38:17 +0300

    LW - IP.

--- logicAndWit/www/js/question/whatAmIQuestions.js
+++ logicAndWit/www/js/question/whatAmIQuestions.js
@@ -12,325 +12,325 @@
 		questions: [
 
 			{
-				title: '1',
+				title: 'Question #1',
 				text: 'What do you have to give before you can keep it?',
 				
 				answer: 'Your Word.'
 			},
 			{
-				title: '2',
+				title: 'Question #2',
 				text: 'What can you break without touching it?',
 				
 				answer: 'A promise.'
 			},
 			{
-				title: '3',
+				title: 'Question #3',
 				text: 'What is so fragile that sound can destroy it?',
 				
 				answer: 'Silence.'
 			},
 			{
-				title: '4',
+				title: 'Question #4',
 				text: 'The more you take, the more you leave behind. What are they?',
 				
 				answer: 'Footsteps.'
 			},
 			{
-				title: '5',
+				title: 'Question #5',
 				text: 'What goes up but never comes down?',
 				
 				answer: 'Your age.'
 			},
 			{
-				title: '6',
+				title: 'Question #6',
 				text: 'What\'s always coming but yet never arrives?',
 				
 				answer: 'Tomorrow.'
 			},
 			{
-				title: '7',
+				title: 'Question #7',
 				text: 'What do you throw out when you want to use it, but take in when you don\'t want to use it?',
 				
 				answer: 'An anchor.'
 			},
 			{
-				title: '8',
+				title: 'Question #8',
 				text: 'What grows down while it grows up?',
 				
 				answer: 'A goose.'
 			},
 			{
-				title: '9',
+				title: 'Question #9',
 				text: 'When I was born I was big but the older I get the smaller I am. What am I?',
 				
 				answer: 'A candle.'
 			},
 			{
-				title: '10',
+				title: 'Question #10',
 				text: 'What is greater than God, more evil than the devil, the poor have it, the rich need it and if you eat it you\'ll die?',
 				
 				answer: 'Nothing.'
 			},
 			{
-				title: '11',
+				title: 'Question #11',
 				text: 'I live where light is but die if light touches me. What am I?',
 				
 				answer: 'A shadow.'
 			},
 			{
-				title: '12',
+				title: 'Question #12',
 				text: 'What gets smaller when ideas grow?',
 				
 				answer: 'A pencil.'
 			},
 			{
-				title: '13',
+				title: 'Question #13',
 				text: 'What is yours to own, yet others use it more?',
 				
 				answer: 'Your name.'
 			},
 			{
-				title: '14',
+				title: 'Question #14',
 				text: 'What loses its head every morning; but gets it back every night?',
 				
 				answer: 'A pillow.'
 			},
 			{
-				title: '15',
+				title: 'Question #15',
 				text: 'What goes all around town but never comes inside?',
 				
 				answer: 'The street.'
 			},
 			{
-				title: '16',
+				title: 'Question #16',
 				text: 'What has holes but holds water?',
 				
 				answer: 'A sponge.'
 			},
 			{
-				title: '17',
+				title: 'Question #17',
 				text: 'What starts with a P and ends with an E and has a thousand letters in it?',
 				
 				answer: 'The post office.'
 			},
 			{
-				title: '18',
+				title: 'Question #18',
 				text: 'What breaks and never falls and what falls and never breaks?',
 				
 				answer: 'Day breaks and night falls.'
 			},
 			{
-				title: '19',
+				title: 'Question #19',
 				text: 'I\'m as light as a feather yet an average man can\'t hold me more than one minute. What am I?',
 				
 				answer: 'Your breath.'
 			},
 			{
-				title: '20',
+				title: 'Question #20',
 				text: 'I have lots of keys but I can\'t open anything. What am I?',
 				
 				answer: 'A piano.'
 			},
 			{
-				title: '21',
+				title: 'Question #21',
 				text: 'What is something that you can take from and then it will get bigger?',
 				
 				answer: 'A piano.'
 			},
 			{
-				title: '21',
+				title: 'Question #21',
 				text: 'What can you feed but if you give it water it will die?',
 				
 				answer: 'A fire.'
 			},
 			{
-				title: '22',
+				title: 'Question #22',
 				text: 'What runs around a soccer field but never moves?',
 				
 				answer: 'A fence.'
 			},
 			{
-				title: '23',
+				title: 'Question #23',
 				text: 'What has rivers but no water, forests but no trees, and cities but no people?',
 				
 				answer: 'A map.'
 			},
 			{
-				title: '24',
+				title: 'Question #24',
 				text: 'What has a tongue that can\'t speak, eyes that can\'t see and a soul that can\'t be saved?',
 				
 				answer: 'A shoe.'
 			},
 			{
-				title: '25',
+				title: 'Question #25',
 				text: 'What is bare in the winter and covered in the summer?',
 				
 				answer: 'A tree.'
 			},
 			{
-				title: '27',
+				title: 'Question #27',
 				text: 'What has ears but can\'t hear?',
 				
 				answer: 'Corn.'
 			},
 			{
-				title: '28',
+				title: 'Question #28',
 				text: 'You can feel it, but you can\'t touch it. You can hear it, but you can\'t see it. What is it?',
 				
 				answer: 'The wind.'
 			},
 			{
-				title: '29',
+				title: 'Question #29',
 				text: 'I start with an E, end with an E and I only have one letter, what am I?',
 				
 				answer: 'An envelope.'
 			},
 			{
-				title: '30',
+				title: 'Question #30',
 				text: 'What has 3 feet and nothing else?',
 				
 				answer: 'A yardstick.'
 			},
 			{
-				title: '31',
+				title: 'Question #31',
 				text: 'What has four legs and a back, but can\'t walk?',
 				
 				answer: 'A chair.'
 			},
 			{
-				title: '32',
+				title: 'Question #32',
 				text: 'What is deaf, dumb and blind, but always tells the truth?',
 				
 				answer: 'A mirror.'
 			},
 			{
-				title: '33',
+				title: 'Question #33',
 				text: 'What goes up and down without moving?',
 				
 				answer: 'The temperature.'
 			},
 			{
-				title: '34',
+				title: 'Question #34',
 				text: 'What has two arms and a neck but no head?',
 				
 				answer: 'A sweater.'
 			},
 			{
-				title: '35',
+				title: 'Question #35',
 				text: 'Who made it, tells it not. Who knows it, wants it not. Who doesn\'t know it, wants it. What is it?',
 				
 				answer: 'Counterfeit money.'
 			},
 			{
-				title: '36',
+				title: 'Question #36',
 				text: 'What can be opened but can\'t be closed?',
 				
 				answer: 'An egg.'
 			},
 			{
-				title: '37',
+				title: 'Question #37',
 				text: 'What has a spine but no bones?',
 				
 				answer: 'A book.'
 			},
 			{
-				title: '38',
+				title: 'Question #38',
 				text: 'What has a face and arms but no nose, ears, mouth, eyes, or a body?',
 				
 				answer: 'A clock.'
 			},
 			{
-				title: '39',
+				title: 'Question #39',
 				text: 'In the dark they are found without being fetched, in the light they are lost without being stolen. What are they?',
 				
 				answer: 'Stars.'
 			},
 			{
-				title: '40',
+				title: 'Question #40',
 				text: 'What runs but never walks, has a mouth that never talks?',
 				
 				answer: 'A river.'
 			},
 			{
-				title: '41',
+				title: 'Question #41',
 				text: 'What has 4 legs at the beginning of its life, 2 in the middle and 3 at the almost end of its life?',
 				
 				answer: 'A human: crawling, walking, using a cane.'
 			},
 			{
-				title: '42',
+				title: 'Question #42',
 				text: 'What gets white as it gets dirty?',
 				
 				answer: 'A chalkboard.'
 			},
 			{
-				title: '42',
+				title: 'Question #42',
 				text: 'What has teeth but can\'t bite?',
 				
 				answer: 'A comb.'
 			},
 			{
-				title: '43',
+				title: 'Question #43',
 				text: 'What has four teeth and a tail, but no arms and legs?',
 				
 				answer: 'A fork.'
 			},
 			{
-				title: '44',
+				title: 'Question #44',
 				text: 'You buy it to eat, but you don\'t eat it. What is it?',
 				
 				answer: 'The plate.'
 			},
 			{
-				title: '45',
+				title: 'Question #45',
 				text: 'I can fly without wings. And cry without eyes. What am I?',
 				
 				answer: 'A cloud.'
 			},
 			{
-				title: '46',
+				title: 'Question #46',
 				text: 'What word in the English language uses all five vowels plus Y in alphabetical order and uses each one only once?',
 				
 				answer: 'Facetiously, which means not seriously.'
 			},
 			{
-				title: '47',
+				title: 'Question #47',
 				text: 'What does not live but can die?',
 				
 				answer: 'A battery.'
 			},
 			{
-				title: '48',
+				title: 'Question #48',
 				text: 'What has roots that nobody sees, is taller than a tree, up, up it goes and yet never grows?',
 				
 				answer: 'A mountain.'
 			},
 			{
-				title: '49',
+				title: 'Question #49',
 				text: 'I\'m hard all around but soft in the middle. What am I?',
 				
 				answer: 'A bed.'
 			},
 			{
-				title: '50',
+				title: 'Question #50',
 				text: 'I hang or stand by a wall, run fast with hands but no feet at all. What am I?',
 				
 				answer: 'A clock.'
 			},
 			{
-				title: '51',
+				title: 'Question #51',
 				text: 'Rough and gray as rock, I\'m plain as plain can be. But hidden deep inside there\'s great beauty in me. What am I?',
 				
 				answer: 'An oyster.'
 			},
 			{
-				title: '52',
+				title: 'Question #52',
 				text: 'I have four wings, but cannot fly, I never laugh and never cry; On the same spot I\'m always found, toiling away with little sound. What am I?',
 				
 				answer: 'A windmill.'
 			},
 			{
-				title: '53',
+				title: 'Question #53',
 				text: 'What is as big as an elephant but weighs nothing?',
 				
 				answer: 'An elephant\'s shadow.'
