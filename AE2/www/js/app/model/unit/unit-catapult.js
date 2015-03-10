ed5ad38f5b689649d01885ce95b8fb74cb2b7092
webbestmaster <web.best.master@gmail.com>
2015-03-10 22:37:54 +0300

    ae2 - ip.

---
+++ AE2/www/js/app/model/unit/unit-catapult.js
@@ -0,0 +1,13 @@
+(function (win) {
+
+	"use strict";
+	/*global console, alert, window, document */
+	/*global */
+
+	win.APP.BB.Unit.CatapultModel =  win.APP.BB.Unit.BaseUnitModel.extend({
+
+		defaults: win.APP.unitMaster.list.catapult
+
+	});
+
+}(window));
