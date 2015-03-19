316dac5a38908ee37f0c500cb08c5784ebe23aba
webbestmaster <web.best.master@gmail.com>
2015-03-19 23:22:26 +0300

    ae2 - sorceress.

--- AE2/www/js/app/model/unit/unit-sorceress.js
+++ AE2/www/js/app/model/unit/unit-sorceress.js
@@ -4,9 +4,29 @@
 	/*global console, alert, window, document */
 	/*global */
 
+	var proto = win.APP.BB.Unit.BaseUnitModel.prototype;
+
 	win.APP.BB.Unit.SorceressModel =  win.APP.BB.Unit.BaseUnitModel.extend({
 
-		defaults: win.APP.unitMaster.list.sorceress
+		defaults: win.APP.unitMaster.list.sorceress,
+
+		getAvailablePathWithTeamUnit: function () {
+
+			var unit = this,
+				model = unit.get('model'),
+				graves = model.get('graves'),
+				path,
+				endPath = [];
+
+			path = proto.getAvailablePathWithTeamUnit.apply(unit, arguments);
+
+			_.each(path, function (xy) {
+				return _.find(graves, xy) || endPath.push(xy);
+			});
+
+			return endPath;
+
+		}
 
 	});
 
