b3ebbc54a1d9a3e305039277edfa0031f4a931c0
webbestmaster <web.best.master@gmail.com>
2015-05-23 16:26:02 +0200

    ae2

---
+++ other/remove-devices/devices.js
@@ -0,0 +1,862 @@
+window.devices = [
+  {
+    "product": "Z120",
+    "deviceName": "Z2"
+  },
+  {
+    "product": "Liquid Z3",
+    "deviceName": "acer_ZXR"
+  },
+  {
+    "product": "Z110",
+    "deviceName": "Z1"
+  },
+  {
+    "product": "Liquid Z3",
+    "deviceName": "ZX"
+  },
+  {
+    "product": "Philips W336",
+    "deviceName": "Crane"
+  },
+  {
+    "product": "MeMO PAD",
+    "deviceName": "me172v"
+  },
+  {
+    "product": "MegaFon MT3A",
+    "deviceName": "MT3A"
+  },
+  {
+    "product": "CINK SLIM",
+    "deviceName": "s8073"
+  },
+  {
+    "product": "Fly EVO Chic 1",
+    "deviceName": "IQ4405_Quad"
+  },
+  {
+    "product": "HTC One SV",
+    "deviceName": "k2plccl"
+  },
+  {
+    "product": "HTC Explorer A310e",
+    "deviceName": "pico"
+  },
+  {
+    "product": "Hero",
+    "deviceName": "heroc"
+  },
+  {
+    "product": "HTC One VX",
+    "deviceName": "totemc2"
+  },
+  {
+    "product": "HTC One V",
+    "deviceName": "primoc"
+  },
+  {
+    "product": "HTC One max",
+    "deviceName": "t6whl"
+  },
+  {
+    "product": "HTC One X+",
+    "deviceName": "evitareul"
+  },
+  {
+    "product": "HTC One X",
+    "deviceName": "endeavoru"
+  },
+  {
+    "product": "HTC One max",
+    "deviceName": "t6ul"
+  },
+  {
+    "product": "HTC One V",
+    "deviceName": "primou"
+  },
+  {
+    "product": "Wildfire S",
+    "deviceName": "marvel"
+  },
+  {
+    "product": "HTC One S Special Edition",
+    "deviceName": "villeplus"
+  },
+  {
+    "product": "HTC One SV",
+    "deviceName": "k2ul"
+  },
+  {
+    "product": "HTC Desire C",
+    "deviceName": "golfu"
+  },
+  {
+    "product": "ADR6325",
+    "deviceName": "lexikon"
+  },
+  {
+    "product": "Legend",
+    "deviceName": "legend"
+  },
+  {
+    "product": "HTC Desire 200",
+    "deviceName": "gtou"
+  },
+  {
+    "product": "HTC One X+",
+    "deviceName": "enrc2b"
+  },
+  {
+    "product": "HTC One S",
+    "deviceName": "villec2"
+  },
+  {
+    "product": "HTC One",
+    "deviceName": "m7wlv"
+  },
+  {
+    "product": "HTC One XL",
+    "deviceName": "evita"
+  },
+  {
+    "product": "HTC One SV",
+    "deviceName": "k2cl"
+  },
+  {
+    "product": "HTC One S",
+    "deviceName": "ville"
+  },
+  {
+    "product": "Incredible S",
+    "deviceName": "vivo"
+  },
+  {
+    "product": "HTC Desire C",
+    "deviceName": "golfc"
+  },
+  {
+    "product": "HTC One",
+    "deviceName": "m7cdug"
+  },
+  {
+    "product": "HTC One",
+    "deviceName": "m7cdwg"
+  },
+  {
+    "product": "HTC One",
+    "deviceName": "m7wls"
+  },
+  {
+    "product": "HTCEVODesign4G",
+    "deviceName": "kingdom"
+  },
+  {
+    "product": "HTC One mini",
+    "deviceName": "htc_m4"
+  },
+  {
+    "product": "HTC One SC",
+    "deviceName": "cp2dcg"
+  },
+  {
+    "product": "HTC ONE SV",
+    "deviceName": "k2u"
+  },
+  {
+    "product": "MEGAFON MS3A",
+    "deviceName": "Megafon"
+  },
+  {
+    "product": "G610-U20",
+    "deviceName": "hwG610-U20"
+  },
+  {
+    "product": "Y301A1",
+    "deviceName": "hwY301A1"
+  },
+  {
+    "product": "Y301A2",
+    "deviceName": "hwY301A2"
+  },
+  {
+    "product": "MediaPad 10 FHD",
+    "deviceName": "hws10101u"
+  },
+  {
+    "product": "Event",
+    "deviceName": "C5133"
+  },
+  {
+    "product": "LG Optimus L4 II Tri",
+    "deviceName": "vee4ts"
+  },
+  {
+    "product": "LG Optimus L7",
+    "deviceName": "u0"
+  },
+  {
+    "product": "LG Optimus L5 II",
+    "deviceName": "vee5ds"
+  },
+  {
+    "product": "LG Optimus L1II",
+    "deviceName": "v1"
+  },
+  {
+    "product": "LG Optimus L3 II",
+    "deviceName": "vee3ds"
+  },
+  {
+    "product": "LG Optimus L5 II",
+    "deviceName": "vee5ss"
+  },
+  {
+    "product": "LG Optimus L4 II Dual",
+    "deviceName": "vee4ds"
+  },
+  {
+    "product": "Swift",
+    "deviceName": "swift"
+  },
+  {
+    "product": "LG Optimus L4 II",
+    "deviceName": "vee4ss"
+  },
+  {
+    "product": "Optimus L5",
+    "deviceName": "m4"
+  },
+  {
+    "product": "LG Optimus L3 II",
+    "deviceName": "vee3e"
+  },
+  {
+    "product": "LG Optimus L1II",
+    "deviceName": "v1ts"
+  },
+  {
+    "product": "LG Optimus L7II",
+    "deviceName": "vee7ds"
+  },
+  {
+    "product": "LG Optimus L7 II",
+    "deviceName": "vee7e"
+  },
+  {
+    "product": "LG Optimus L1II",
+    "deviceName": "v1ds"
+  },
+  {
+    "product": "LG Optimus L5 II",
+    "deviceName": "vee5nfc"
+  },
+  {
+    "product": "Optimus One",
+    "deviceName": "thunderg"
+  },
+  {
+    "product": "Primo81",
+    "deviceName": "N821"
+  },
+  {
+    "product": "Moto Defy XT",
+    "deviceName": "tinboostplus_umts"
+  },
+  {
+    "product": "Defy",
+    "deviceName": "umts_jordan"
+  },
+  {
+    "product": "Droid X",
+    "deviceName": "cdma_shadow"
+  },
+  {
+    "product": "RAZR D1",
+    "deviceName": "hawk35_umts"
+  },
+  {
+    "product": "CASIO G'zOne Commando 4G LTE",
+    "deviceName": "C811"
+  },
+  {
+    "product": "Casio G'zOne Commando",
+    "deviceName": "C771"
+  },
+  {
+    "product": "ITP-R208W",
+    "deviceName": "rk30sdk"
+  },
+  {
+    "product": "Galaxy Young",
+    "deviceName": "roydtv"
+  },
+  {
+    "product": "Galaxy Y",
+    "deviceName": "GT-S5360B"
+  },
+  {
+    "product": "Galaxy Trend Duos",
+    "deviceName": "kyleichn"
+  },
+  {
+    "product": "Galaxy Ace Duos",
+    "deviceName": "GT-S6802B"
+  },
+  {
+    "product": "Galaxy Ace",
+    "deviceName": "GT-S5830i"
+  },
+  {
+    "product": "Galaxy Note 10.1",
+    "deviceName": "p4noterf"
+  },
+  {
+    "product": "Galaxy Ace",
+    "deviceName": "GT-S5830C"
+  },
+  {
+    "product": "Galaxy Ace Plus",
+    "deviceName": "GT-S7500L"
+  },
+  {
+    "product": "Galaxy Trend",
+    "deviceName": "kyleve3gcmcc"
+  },
+  {
+    "product": "Galaxy Star",
+    "deviceName": "mintss"
+  },
+  {
+    "product": "Galaxy Ace",
+    "deviceName": "GT-S5839i"
+  },
+  {
+    "product": "Galaxy Mega 6.3",
+    "deviceName": "melius3gduosctc"
+  },
+  {
+    "product": "Galaxy Trend Lite",
+    "deviceName": "kylevess"
+  },
+  {
+    "product": "Galaxy Ace",
+    "deviceName": "GT-S5830M"
+  },
+  {
+    "product": "Galaxy Fame",
+    "deviceName": "nevisp"
+  },
+  {
+    "product": "Galaxy Fit",
+    "deviceName": "GT-S5670"
+  },
+  {
+    "product": "Galaxy Music Duos",
+    "deviceName": "ivory"
+  },
+  {
+    "product": "Europa",
+    "deviceName": "GT-I5500L"
+  },
+  {
+    "product": "Galaxy Y",
+    "deviceName": "GT-S5360L"
+  },
+  {
+    "product": "Galaxy Young",
+    "deviceName": "roy"
+  },
+  {
+    "product": "Galaxy Tab 8.9",
+    "deviceName": "SGH-I957D"
+  },
+  {
+    "product": "Galaxy Fame",
+    "deviceName": "nevis"
+  },
+  {
+    "product": "Galaxy Trend",
+    "deviceName": "kyleve"
+  },
+  {
+    "product": "Galaxy Ace",
+    "deviceName": "GT-S5830B"
+  },
+  {
+    "product": "Galaxy Pocket",
+    "deviceName": "GT-S5302"
+  },
+  {
+    "product": "Galaxy Pocket Neo",
+    "deviceName": "corsicass"
+  },
+  {
+    "product": "Galaxy Mega 6.3",
+    "deviceName": "melius3g"
+  },
+  {
+    "product": "Galaxy Fit",
+    "deviceName": "GT-S5670L"
+  },
+  {
+    "product": "Galaxy Tab2 10.1",
+    "deviceName": "espresso10rf"
+  },
+  {
+    "product": "Galaxy Fame",
+    "deviceName": "nevisds"
+  },
+  {
+    "product": "Galaxy Young",
+    "deviceName": "royssdtv"
+  },
+  {
+    "product": "Galaxy Tab 8.9",
+    "deviceName": "SGH-I957M"
+  },
+  {
+    "product": "Galaxy Ace",
+    "deviceName": "GT-S5830"
+  },
+  {
+    "product": "Galaxy Fame",
+    "deviceName": "nevisvess"
+  },
+  {
+    "product": "Galaxy Ace",
+    "deviceName": "SHW-M240S"
+  },
+  {
+    "product": "Galaxy Fame Lite Duos",
+    "deviceName": "nevisw"
+  },
+  {
+    "product": "Galaxy S3 Mini",
+    "deviceName": "goldenltebmc"
+  },
+  {
+    "product": "Galaxy Y Duos",
+    "deviceName": "GT-S6102"
+  },
+  {
+    "product": "Galaxy Tab 10.1",
+    "deviceName": "GT-P7510"
+  },
+  {
+    "product": "Galaxy Y",
+    "deviceName": "GT-S5369"
+  },
+  {
+    "product": "Galaxy Y",
+    "deviceName": "GT-S5360"
+  },
+  {
+    "product": "Galaxy Ace Duos",
+    "deviceName": "GT-S6802"
+  },
+  {
+    "product": "Galaxy Mini",
+    "deviceName": "GT-S5570B"
+  },
+  {
+    "product": "Galaxy Ace",
+    "deviceName": "GT-S5830L"
+  },
+  {
+    "product": "Galaxy Mini",
+    "deviceName": "GT-S5570I"
+  },
+  {
+    "product": "Galaxy Tab 8.9",
+    "deviceName": "SHW-M305W"
+  },
+  {
+    "product": "Galaxy Y TV",
+    "deviceName": "GT-S5367"
+  },
+  {
+    "product": "SHW-M480S",
+    "deviceName": "p4noterfskt"
+  },
+  {
+    "product": "Galaxy Trend Plus",
+    "deviceName": "kylepro"
+  },
+  {
+    "product": "Galaxy S Plus",
+    "deviceName": "GT-I9001"
+  },
+  {
+    "product": "Galaxy Mini",
+    "deviceName": "GT-S5570L"
+  },
+  {
+    "product": "Galaxy Pocket",
+    "deviceName": "GT-S5302B"
+  },
+  {
+    "product": "Galaxy Y Pro",
+    "deviceName": "GT-B5510"
+  },
+  {
+    "product": "Galaxy S3 Mini",
+    "deviceName": "goldenlteatt"
+  },
+  {
+    "product": "Galaxy Young",
+    "deviceName": "royssnfc"
+  },
+  {
+    "product": "Galaxy Fame",
+    "deviceName": "nevis3gcmcc"
+  },
+  {
+    "product": "Galaxy Pocket",
+    "deviceName": "GT-S5300"
+  },
+  {
+    "product": "Galaxy Y Pro",
+    "deviceName": "GT-B5510L"
+  },
+  {
+    "product": "Galaxy Pocket Neo",
+    "deviceName": "corsica"
+  },
+  {
+    "product": "Galaxy S2",
+    "deviceName": "SHW-M250L"
+  },
+  {
+    "product": "Galaxy Star Trios",
+    "deviceName": "mintts"
+  },
+  {
+    "product": "Galaxy Fame",
+    "deviceName": "nevisnvess"
+  },
+  {
+    "product": "Galaxy Fame",
+    "deviceName": "nevis3g"
+  },
+  {
+    "product": "Galaxy Pocket Plus",
+    "deviceName": "coriplus"
+  },
+  {
+    "product": "Galaxy Note 10.1",
+    "deviceName": "p4noterfktt"
+  },
+  {
+    "product": "Galaxy Star",
+    "deviceName": "mint"
+  },
+  {
+    "product": "Galaxy Mini",
+    "deviceName": "SGH-T499"
+  },
+  {
+    "product": "Galaxy S3 Mini",
+    "deviceName": "goldenltevzw"
+  },
+  {
+    "product": "Galaxy Mini2",
+    "deviceName": "GT-S6500"
+  },
+  {
+    "product": "Galaxy S2",
+    "deviceName": "SHW-M250S"
+  },
+  {
+    "product": "Galaxy Pocket",
+    "deviceName": "GT-S5300B"
+  },
+  {
+    "product": "Galaxy S DUOS",
+    "deviceName": "kyleopen"
+  },
+  {
+    "product": "Galaxy Y Plus",
+    "deviceName": "coriplusds"
+  },
+  {
+    "product": "Galaxy Y Duos",
+    "deviceName": "GT-S6102B"
+  },
+  {
+    "product": "Galaxy Note 10.1",
+    "deviceName": "p4notelteskt"
+  },
+  {
+    "product": "Galaxy S2",
+    "deviceName": "SHW-M250K"
+  },
+  {
+    "product": "Galaxy S",
+    "deviceName": "GT-I9000B"
+  },
+  {
+    "product": "Galaxy S3 Mini",
+    "deviceName": "golden"
+  },
+  {
+    "product": "Galaxy S",
+    "deviceName": "GT-I9003"
+  },
+  {
+    "product": "Galaxy Mini",
+    "deviceName": "GT-S5570"
+  },
+  {
+    "product": "Xperia active",
+    "deviceName": "ST17i"
+  },
+  {
+    "product": "Xperia Go",
+    "deviceName": "ST27a"
+  },
+  {
+    "product": "Xperia E dual",
+    "deviceName": "C1605"
+  },
+  {
+    "product": "Xperia X10",
+    "deviceName": "X10i"
+  },
+  {
+    "product": "Xperia X8",
+    "deviceName": "E15i"
+  },
+  {
+    "product": "Xperia PLAY",
+    "deviceName": "R800a"
+  },
+  {
+    "product": "Xperia mini pro",
+    "deviceName": "SK17a"
+  },
+  {
+    "product": "Xperia X10 Mini Pro",
+    "deviceName": "U20i"
+  },
+  {
+    "product": "R800x",
+    "deviceName": "R800x"
+  },
+  {
+    "product": "Xperia Go",
+    "deviceName": "ST27i"
+  },
+  {
+    "product": "Xperia J",
+    "deviceName": "ST26i"
+  },
+  {
+    "product": "Xperia PLAY",
+    "deviceName": "R800i"
+  },
+  {
+    "product": "Xperia arc S",
+    "deviceName": "LT18a"
+  },
+  {
+    "product": "X10 Xperia Mini",
+    "deviceName": "robyn"
+  },
+  {
+    "product": "Xperia Tipo",
+    "deviceName": "ST21i2"
+  },
+  {
+    "product": "Xperia mini",
+    "deviceName": "ST15i"
+  },
+  {
+    "product": "Xperia arc",
+    "deviceName": "LT15i"
+  },
+  {
+    "product": "Xperia Miro",
+    "deviceName": "ST23i"
+  },
+  {
+    "product": "Xperia E",
+    "deviceName": "C1504"
+  },
+  {
+    "product": "Xperia Tipo",
+    "deviceName": "ST21i"
+  },
+  {
+    "product": "Xperia ray",
+    "deviceName": "ST18i"
+  },
+  {
+    "product": "Xperia mini",
+    "deviceName": "ST15a"
+  },
+  {
+    "product": "ALCATEL ONE TOUCH 5020D",
+    "deviceName": "Megane_GSM"
+  },
+  {
+    "product": "ONE TOUCH 4011X",
+    "deviceName": "BeetleliteJB"
+  },
+  {
+    "product": "one_touch_4030_TLVE",
+    "deviceName": "Beetle_GSM"
+  },
+  {
+    "product": "Megafon Login",
+    "deviceName": "OT-930"
+  },
+  {
+    "product": "ZTE N880F",
+    "deviceName": "ZTE-N880F"
+  },
+  {
+    "product": "ZTE Blade Q Mini",
+    "deviceName": "P172G10_UK_VIRGIN"
+  },
+  {
+    "product": "N850L",
+    "deviceName": "seanplus"
+  },
+  {
+    "product": "N810",
+    "deviceName": "fluid"
+  },
+  {
+    "product": "ZTE N881F",
+    "deviceName": "ZTE-N881F"
+  },
+  {
+    "product": "ZTE N818",
+    "deviceName": "ZTE-N818"
+  },
+  {
+    "product": "ZTE N807",
+    "deviceName": "ZTE-N807"
+  },
+  {
+    "product": "N861",
+    "deviceName": "warp2"
+  },
+  {
+    "product": "N860",
+    "deviceName": "arthur"
+  },
+  {
+    "product": "ZTE N880G",
+    "deviceName": "ZTE-N880G"
+  },
+  {
+    "product": "ZTE Blade III Pro",
+    "deviceName": "Blade-III-Pro"
+  },
+  {
+    "product": "ZTE N881E",
+    "deviceName": "ZTE-N881E"
+  },
+  {
+    "product": "ZTE Blade G",
+    "deviceName": "ZTE-Blade-G"
+  },
+  {
+    "product": "ZTE V795",
+    "deviceName": "P172D01"
+  },
+  {
+    "product": "N850",
+    "deviceName": "sean"
+  },
+  {
+    "product": "N800",
+    "deviceName": "nex"
+  },
+  {
+    "product": "V880",
+    "deviceName": "blade"
+  },
+  {
+    "product": "Blade G LTE",
+    "deviceName": "oceanus"
+  },
+  {
+    "product": "ZTE Blade V",
+    "deviceName": "ZTE-Blade-V"
+  },
+  {
+    "product": null,
+    "deviceName": "hsdroid"
+  },
+  {
+    "product": null,
+    "deviceName": "rk2928sdk"
+  },
+  {
+    "product": null,
+    "deviceName": "bq_Edison2_QuadCore"
+  },
+  {
+    "product": null,
+    "deviceName": "rk2928sk7240"
+  },
+  {
+    "product": null,
+    "deviceName": "GLORY"
+  },
+  {
+    "product": null,
+    "deviceName": "Fly"
+  },
+  {
+    "product": null,
+    "deviceName": "Golf"
+  },
+  {
+    "product": null,
+    "deviceName": "gs701b"
+  },
+  {
+    "product": null,
+    "deviceName": "crane-inet"
+  },
+  {
+    "product": null,
+    "deviceName": "SmartPad Go"
+  },
+  {
+    "product": null,
+    "deviceName": "Fly_IQ454"
+  },
+  {
+    "product": null,
+    "deviceName": "rkm755sdk"
+  },
+  {
+    "product": null,
+    "deviceName": "s8073b"
+  },
+  {
+    "product": null,
+    "deviceName": "MegaFon_SP-AI"
+  },
+  {
+    "product": null,
+    "deviceName": "rk2928sk7204"
+  },
+  {
+    "product": null,
+    "deviceName": "NX008HD8G"
+  },
+  {
+    "product": null,
+    "deviceName": "Fly_IQ4404"
+  },
+  {
+    "product": null,
+    "deviceName": "cooper"
+  }
+]
--- other/remove-devices/disabler.js
+++ other/remove-devices/disabler.js
@@ -8,9 +8,7 @@
 	var disabler = {
 
 		selectors: {
-			itemWrapper: '[data-device-id]',
-			deviceName: '[data-device-name]',
-			product: '[data-product]'
+			searchInput: '.DWI0QJD-cc-d'
 		},
 
 		findAll: function (selector, context) {
@@ -29,8 +27,87 @@
 
 			return Array.prototype.slice.call(list);
 
-		}
+		},
 
+		dispatchChange: function (data) {
+
+			var node = data.node,
+				evt = new Event('change');
+
+			node.dispatchEvent(evt);
+
+		},
+
+		dispatchClick: function (data) {
+
+			var node = data.node,
+				evt = doc.createEvent('MouseEvents');
+
+			evt.initEvent('click', true, true );
+
+			node.dispatchEvent(evt);
+
+		},
+
+		disableDevice: function (data) {
+
+			var disabler = this,
+				deferred = Promise.defer(),
+				deviceWrapper,
+				deviceName = data.deviceName,
+				selectors = disabler.selectors,
+				input = disabler.find(selectors.searchInput);
+
+				input.value = deviceName;
+				disabler.dispatchChange({ node: input });
+
+			setTimeout(function () {
+				deviceWrapper = disabler.find('[data-device-id="' + deviceName + '"]');
+
+				if (!deviceWrapper) {
+					console.log(deviceName + ' is not found');
+					return;
+				}
+
+				var checkbox = disabler.find('checkbox', deviceWrapper);
+
+				if (checkbox.getAttribute('aria-disabled') === 'true') {
+					console.log(deviceName + ' - disabled by google');
+					return;
+				}
+
+				if (checkbox.getAttribute('aria-checked') === 'true') {
+					return;
+				}
+
+				disabler.dispatchClick({ node: checkbox });
+
+			}, 100);
+
+			setTimeout(function () {
+				deferred.resolve();
+			}, 500);
+
+			return deferred.promise;
+
+		},
+
+		disableDevices: function () {
+
+			var disabler = this,
+				deferred = Promise.defer(),
+				promise = deferred.promise,
+				devices = window.devices;
+
+			devices.forEach(function (data) {
+				promise = promise.then(function () {
+					return disabler.disableDevice(data);
+				});
+			});
+
+			deferred.resolve();
+
+		}
 
 	};
 
