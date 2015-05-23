da048461e92ae1416c3870ef92a3453116d99f47
webbestmaster <web.best.master@gmail.com>
2015-05-23 10:55:18 +0200

    ae2

--- other/remove-devices/disabler.js
+++ other/remove-devices/disabler.js
@@ -1,3 +1,39 @@
-/**
- * Created by user on 23.05.2015.
- */
+/*jslint white: true, nomen: true */
+(function (win, doc) {
+
+	'use strict';
+	/*global window */
+	/*global */
+
+	var disabler = {
+
+		selectors: {
+			itemWrapper: '[data-device-id]',
+			deviceName: '[data-device-name]',
+			product: '[data-product]'
+		},
+
+		findAll: function (selector, context) {
+
+			return this.toArray((context || doc).querySelectorAll(selector));
+
+		},
+
+		find: function (selector, context) {
+
+			return (context || doc).querySelector(selector);
+
+		},
+
+		toArray: function (list) {
+
+			return Array.prototype.slice.call(list);
+
+		}
+
+
+	};
+
+	win.disabler = disabler;
+
+}(window, window.document));
