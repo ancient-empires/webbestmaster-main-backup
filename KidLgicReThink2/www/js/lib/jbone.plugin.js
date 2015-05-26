3832b011ecd62f03e0e0cd819c22a4eeeda2657a
webbestmaster <web.best.master@gmail.com>
2015-05-26 17:58:38 +0300

    kl

---
+++ KidLgicReThink2/www/js/lib/jbone.plugin.js
@@ -0,0 +1,28 @@
+/*jslint white: true, nomen: true */
+(function (win) {
+
+	"use strict";
+	/*global window */
+	/*global */
+
+	/*
+
+	Example
+
+	 jBone.fn.addClass = function(className) {
+	 var i = 0,
+	 length = this.length;
+
+	 for (; i < length; i++) {
+	 this[i].classList.add(className);
+	 }
+
+	 return this;
+	 };
+
+	 $(".header").addClass("loaded");
+
+
+	*/
+
+}(window));
---
+++ KidLgicReThink2/www/js/services/log.js
@@ -0,0 +1,28 @@
+(function (win) {
+
+	"use strict";
+	/*global window, document, console */
+	/*global */
+
+	win.logger = {
+		on: function () {
+			console.log('Logger is Enabled');
+			this.isEnable = true;
+		},
+		off: function () {
+			console.log('Logger is Disabled');
+			this.isEnable = false;
+		},
+		isEnable: false,
+		log: function () {
+			return this.isEnable && console.log.apply(console, arguments);
+		}
+	};
+
+	function log() {
+		return logger.log.apply(logger, arguments);
+	}
+
+	win.log = log;
+
+}(window));
