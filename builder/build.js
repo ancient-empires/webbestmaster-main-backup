48e1b11bbac7cff6e4e4d6292dae1c1aa53a4d27
webbestmaster <web.best.master@gmail.com>
2015-05-31 11:52:41 +0200

    ae2

---
+++ builder/build.js
@@ -0,0 +1,43 @@
+/*jslint white: true, nomen: true */
+(function () {
+
+	'use strict';
+	/*global window */
+	/*global */
+
+	var fs = require('fs'),
+
+		html = fs.readFileSync('./www/index.html', 'utf-8'),
+
+		scripts = html.match(/src="js\/\S+"/g),
+
+		endJs = '',
+
+		endHtml = html;
+
+	scripts.forEach(function (src) {
+
+		endHtml = endHtml.replace('<script ' + src + ' type="text/javascript"><\/script>', 'scriptRemoved');
+
+		src = './www/' + src.replace(/^src="|"$/g, '');
+		endJs += fs.readFileSync(src, 'utf-8');
+		fs.unlink(src);
+	});
+
+	endHtml = endHtml.replace('scriptRemoved', '<script src="js/all.js" type="text/javascript"><\/script>');
+
+	endHtml = endHtml.replace(/scriptRemoved[\s\S]+scriptRemoved/g, '');
+
+	fs.appendFile('./www/js/all.js', endJs, function (err) {
+		if (err) throw err;
+		console.log('ALL JS');
+	});
+
+	fs.writeFile('./www/index.html', endHtml, function (err) {
+		if (err) throw err;
+		console.log('INDEX HTML');
+	});
+
+
+
+}());
