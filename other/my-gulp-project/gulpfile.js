43f3246a9816b2571f7053c2aa4e93054ff19476
webbestmaster <web.best.master@gmail.com>
2015-07-07 20:02:17 +0200

    gulp

---
+++ other/my-gulp-project/gulpfile.js
@@ -0,0 +1,29 @@
+/*jslint white: true, nomen: true */
+(function () {
+
+    var gulp = require('gulp');
+    var closureCompiler = require('gulp-closure-compiler');
+
+    // gulp - run 'default' task
+    // gulp <task> <othertask>.
+
+    gulp.task('default', function() {
+
+
+        return gulp.src('src/*.js')
+            .pipe(closureCompiler({
+                compilerPath: 'bower_components/closure-compiler/compiler.jar',
+                fileName: 'build.js'
+            }))
+            .pipe(gulp.dest('dist'));
+
+
+        // place code for your default task here
+
+
+    });
+
+
+
+
+}());
