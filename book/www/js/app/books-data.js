6aa92bc523fe14fc7e18cf3940f3d7b817d78056
webbestmaster <web.best.master@gmail.com>
2015-07-09 14:08:46 +0300

    book

---
+++ book/www/js/app/books-data.js
@@ -0,0 +1,94 @@
+/*jslint white: true, nomen: true */
+(function (win) {
+
+	"use strict";
+	/*global window */
+	/*global */
+
+	win.APP.booksData = {
+
+		'en': [
+			{
+				folder: 'book 0',
+				title: {
+					text: 'title text',
+					img: 'title.jpg'
+				},
+				pages: [
+					{
+						text: 'englist text 1',
+						img: '1.jpg'
+					},
+					{
+						text: 'englist text 2',
+						img: '2.jpg'
+					}
+				]
+			},
+
+
+			{
+				folder: 'book 1',
+				title: {
+					text: 'title text',
+					img: 'title.jpg'
+				},
+				pages: [
+					{
+						text: 'englist text 1',
+						img: '1.jpg'
+					},
+					{
+						text: 'englist text 2',
+						img: '2.jpg'
+					}
+				]
+			},
+			{
+				folder: 'book 2',
+				title: {
+					text: 'title text',
+					img: 'title.jpg'
+				},
+				pages: [
+					{
+						text: 'englist text 1',
+						img: '1.jpg'
+					},
+					{
+						text: 'englist text 2',
+						img: '2.jpg'
+					}
+				]
+			}
+		],
+
+		'ru': [
+			{
+				folder: 'book 0',
+				pages: 3
+			},
+			{
+				folder: 'book 1',
+				pages: 3
+			},
+			{
+				folder: 'book 2',
+				pages: 3
+			}
+		]
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+}(window));
