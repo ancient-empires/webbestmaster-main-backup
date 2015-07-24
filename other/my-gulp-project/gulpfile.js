ae74ae9c15817371d36bc36af5b6c1dbcb222ec2
webbestmaster <web.best.master@gmail.com>
2015-07-24 15:52:38 +0300

    lw

--- other/my-gulp-project/gulpfile.js
+++ other/my-gulp-project/gulpfile.js
@@ -2,24 +2,38 @@
 (function () {
 
     var gulp = require('gulp');
-    var closureCompiler = require('gulp-closure-compiler');
-    //var rename = require("gulp-rename");
+    var rename = require('gulp-rename');
+    //var closureCompiler = require('gulp-closure-compiler');
+    //var minify = require('gulp-minify');
+    var uglify = require("gulp-uglify");
+    var clean = require('gulp-clean');
     // gulp - run 'default' task
     // gulp <task> <othertask>.
 
     gulp.task('default', function() {
+        gulp.run('uglify-js', 'clean');
+    });
+
+    gulp.task('uglify-js', function() {
 
         return gulp.src('www/js/*.js')
-            .pipe(closureCompiler({
-                compilerPath: 'bower_components/closure-compiler/compiler.jar',
-                fileName: 'www/js/all.min.js'
-            }));
+            //.pipe(closureCompiler({
+            //    compilerPath: 'bower_components/closure-compiler/compiler.jar',
+            //    fileName: 'www/js/all.min.js'
+            //}))
+            //.pipe(minify())
+            .pipe(uglify())
+            .pipe(rename('all.min.js'))
+            .pipe(gulp.dest('./www/js'));
 
     });
 
-    gulp.task('remove-extra', function() {
-        var fs = require('fs');
-        fs.unlinkSync('www/js/all.js');
-    });
+    gulp.task('clean', function() {
+
+        return gulp.src('www/js/all.js')
+            .pipe(clean({force: true}));
+
+	});
+
 
 }());
