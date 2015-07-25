1009efebd3e13eb2e92020ae29023d95b34f3853
webbestmaster <web.best.master@gmail.com>
2015-07-25 19:28:20 +0300

    nichosi

---
+++ nichosi/www/js/services/template-master.js
@@ -0,0 +1,40 @@
+/*jslint white: true, nomen: true */
+(function (win, doc) {
+
+	"use strict";
+	/*global window, document */
+	/*global APP, doT */
+
+	win.APP = win.APP || {};
+
+	var templateMaster;
+
+	templateMaster = {
+		templateSelector: 'script[type="text/x-template"]',
+		tmplText: {},
+		tmplFn: {},
+
+		init: function () {
+
+			var templates = doc.querySelectorAll(this.templateSelector);
+
+			Array.prototype.forEach.call(templates, function(tmplNode) {
+
+				var name = tmplNode.getAttribute('data-name'),
+					text = tmplNode.textContent;
+
+				this.tmplText[name] = text;
+				this.tmplFn[name] = doT.template(text);
+
+				tmplNode.parentNode.removeChild(tmplNode);
+
+			}, this);
+
+		}
+
+	};
+
+	win.APP.templateMaster = templateMaster;
+
+}(window, document));
+
