aa8c30368e9e99158b7999491415b71743294bca
webbestmaster <web.best.master@gmail.com>
2015-07-31 16:07:30 +0300

    book

---
+++ other/my-gulp-project/remove-extra-css-to-base64-files.js
@@ -0,0 +1,29 @@
+/*jslint white: true, nomen: true */
+(function () {
+
+	"use strict";
+	/*global window */
+	/*global */
+
+	var fs = require('fs'),
+		css = fs.readFileSync('./www/css/main.css', 'utf-8'),
+		fileList = fs.readdirSync('./www/i/');
+
+	fileList.forEach(function (fileName) {
+
+		if (css.indexOf('../i/' + fileName) !== -1) {
+			return;
+		}
+
+		fs.unlinkSync('./www/i/' + fileName);
+
+	});
+
+
+
+
+
+
+
+
+}());
