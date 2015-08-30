ded717fef9f0d45188db535b9a42bf4d4b8b7f9a
webbestmaster <web.best.master@gmail.com>
2015-08-30 12:09:30 +0300

    eo

--- art-hata/send-request.php
+++ art-hata/send-request.php
@@ -11,17 +11,18 @@ $extra = $_POST['extra'];
 $works_type = $_POST['works-type'];
 
 $mail = new PHPMailer();
-$mail->From = 'arthata.by';      // от кого
-$mail->FromName = 'arthata.by';   // от кого
-$mail->AddAddress('web.best.master@gmail.com', 'arthata.by'); // кому - адрес, Имя
-//$mail->AddAddress('arthataby@gmail.com', 'arthata.by'); // кому - адрес, Имя
-$mail->IsHTML(true);        // выставляем формат письма HTML
-$mail->Subject = $title;  // тема письма
+$mail->CharSet = 'utf-8';
+$mail->From = 'arthata.by';      // îò êîãî
+$mail->FromName = 'arthata.by';   // îò êîãî
+$mail->AddAddress('web.best.master@gmail.com', 'arthata.by'); // êîìó - àäðåñ, Èìÿ
+//$mail->AddAddress('arthataby@gmail.com', 'arthata.by'); // êîìó - àäðåñ, Èìÿ
+$mail->IsHTML(true);        // âûñòàâëÿåì ôîðìàò ïèñüìà HTML
+$mail->Subject = $title;  // òåìà ïèñüìà
 
 $mail->Body = 'title - '.$title.'<br/ >name - '.$name.'<br/ >phone - '.$phone.'<br/ >email - '.$email.'<br/ >content - '.$content.'<br />extra - '.$extra.'<br />works-type - '.$works_type;
 
 // add attachment
-// если было изображение/ия, то прикрепляем его в виде картинки к телу письма.
+// åñëè áûëî èçîáðàæåíèå/èÿ, òî ïðèêðåïëÿåì åãî â âèäå êàðòèíêè ê òåëó ïèñüìà.
 foreach($_FILES as $file) {
     $mail->AddAttachment($file['tmp_name'], $file['name']);
 }
@@ -33,27 +34,28 @@ foreach($_FILES as $file) {
 
 
 
-// отправляем наше письмо
+// îòïðàâëÿåì íàøå ïèñüìî
 //if (!$mail->Send()) die ('Mailer Error: '.$mail->ErrorInfo);
 //echo 'Spasibo! Vash zakaz prinyat.';
 
-// отправляем наше письмо
+// îòïðàâëÿåì íàøå ïèñüìî
 echo 'status:';
 echo $mail->Send();
 echo ';';
 
-$roistatData = array(
-    'roistat' => isset($_COOKIE['roistat_visit']) ? $_COOKIE['roistat_visit'] : null,
-    'key'     => 'NjMzNDo3MDAwOjliZTZhYjdiMzgxMTM1NjE1NWJkMGQ3NTliYjkxMDU1', // Замените SECRET_KEY на секретный ключ из пункта меню Настройки -> Интеграция со сделками в нижней части экрана и строчке Ключ для интеграций
-    'title'   => $title,
-    'comment' => $content,
-    'name'    => $name,
-    'email'   => $email,
-    'phone'   => $phone,
-    'fields'  => array()
-);
 
-//echo "https://cloud.roistat.com/api/proxy/1.0/leads/add?".http_build_query($roistatData);
-echo file_get_contents("https://cloud.roistat.com/api/proxy/1.0/leads/add?".http_build_query($roistatData));
+//$roistatData = array(
+//    'roistat' => isset($_COOKIE['roistat_visit']) ? $_COOKIE['roistat_visit'] : null,
+//    'key'     => 'NjMzNDo3MDAwOjliZTZhYjdiMzgxMTM1NjE1NWJkMGQ3NTliYjkxMDU1', // Çàìåíèòå SECRET_KEY íà ñåêðåòíûé êëþ÷ èç ïóíêòà ìåíþ Íàñòðîéêè -> Èíòåãðàöèÿ ñî ñäåëêàìè â íèæíåé ÷àñòè ýêðàíà è ñòðî÷êå Êëþ÷ äëÿ èíòåãðàöèé
+//    'title'   => $title,
+//    'comment' => $content,
+//    'name'    => $name,
+//    'email'   => $email,
+//    'phone'   => $phone,
+//    'fields'  => array()
+//);
+
+//echo file_get_contents("https://cloud.roistat.com/api/proxy/1.0/leads/add?".http_build_query($roistatData));
+
 
 ?>
