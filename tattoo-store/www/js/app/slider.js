a3f04f3882adabd006129cbb076218f353f0c9f7
webbestmaster <web.best.master@gmail.com>
2015-09-07 16:50:40 +0300

    tattoo

--- tattoo-store/www/js/app/slider.js
+++ tattoo-store/www/js/app/slider.js
@@ -11,8 +11,42 @@
 
 	win.APP.slider = {
 
+		attr: {},
+
+		get: function (key) {
+			return this.attr[key];
+		},
+
+		set: function (key, value) {
+			this.attr[key] = value;
+			return this;
+		},
+
 		initGoodsItem: function () {
 
+			var slider = this,
+				$slides = $('.js-goods-item-swiper-container .swiper-slide'),
+				swiper = new Swiper('.js-goods-item-swiper-container .swiper-container', {
+					//pagination: '.swiper-pagination',
+					slidesPerView: 4,
+					paginationClickable: true,
+					spaceBetween: 0,
+					nextButton: '.js-goods-item-swiper-container .swiper-button-next',
+					prevButton: '.js-goods-item-swiper-container .swiper-button-prev'
+				});
+
+			$slides.on('click', function (e) {
+				var $slideImg = $(e.currentTarget).find('img');
+				$('.js-goods-item-swiper-container .goods-item-slide-selected').removeClass('goods-item-slide-selected');
+				$slideImg.addClass('goods-item-slide-selected');
+				$('.js-goods-item-image').attr('src', $slideImg.attr('src'));
+			});
+
+			$slides.eq(0).trigger('click');
+
+			slider.set('goods-item', swiper);
+
+			return swiper;
 
 		}
 
