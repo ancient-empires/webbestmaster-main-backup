11e8936453aa537c437aca562145aebfaa33c418
webbestmaster <web.best.master@gmail.com>
2015-09-16 17:26:09 +0300

    tattoo

---
+++ art-hata/set-phone.js
@@ -0,0 +1,32 @@
+/*jslint white: true, nomen: true */
+(function (win) {
+
+	"use strict";
+	/*global window */
+	/*global */
+
+	var phoneWatcher = {
+
+		tels: {
+			'23-32-21': ['/'],
+			'23-32-22': ['/'],
+			'23-32-23': ['/']
+		},
+
+		init: function () {
+
+			// get start url
+			// if start url exist in sessionStorage get from sessionStorage
+			// else get url and set to sessionStorage
+
+			// do action in relative from url
+
+
+
+		}
+
+	};
+
+
+
+}(window));
