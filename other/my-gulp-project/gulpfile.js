dc09152e4174824c56502a7429a29e7e1abcb5d7
webbestmaster <web.best.master@gmail.com>
2015-10-27 02:50:21 +0300

    add settings

--- other/my-gulp-project/gulpfile.js
+++ other/my-gulp-project/gulpfile.js
@@ -79,8 +79,8 @@
 
     gulp.task('tiny-png', function () {
         gulp.src(['tinypng/**/*.jpg', 'tinypng/**/*.png', 'tinypng/**/*.jpeg'])
-            //.pipe(tinypng('uY9x_ytUQ0sq9-bB8iTvwGnmiWVci4an')) // web.best.master
-            .pipe(tinypng('RmSQIT1W2KC2_gZf27_KaZ7GWIzpmKJu')) // ae.fan.game 234 * 2 = 468
+            .pipe(tinypng('uY9x_ytUQ0sq9-bB8iTvwGnmiWVci4an')) // web.best.master
+            //.pipe(tinypng('RmSQIT1W2KC2_gZf27_KaZ7GWIzpmKJu')) // ae.fan.game 234 * 2 = 468
             .pipe(gulp.dest('dist'));
     });
 
