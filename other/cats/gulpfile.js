af0b929d486b758077fee4ccd3a9e9d931348e43
webbestmaster <web.best.master@gmail.com>
2015-10-27 14:23:21 +0300

    add settings

--- other/cats/gulpfile.js
+++ other/cats/gulpfile.js
@@ -156,4 +156,17 @@
 		gulp.watch('./www/i/**/*', ['copy-i']);
 	});
 
+	// tiny-png
+	gulp.task('tiny-png', function () {
+		gulp.src(['tinypng/**/*.png', 'tinypng/**/*.jpg', 'tinypng/**/*.jpeg'])
+			.pipe(tinypng('h0DW7VyYVXnl3awj2o7v9wXR-EavOiB5'))
+			// h0DW7VyYVXnl3awj2o7v9wXR-EavOiB5 - kidmathgenius@gmail.com
+			// eSu5nMg0TSDairQWQC_Bx0h41PxKgKEp - mikhail.anisimau.play@gmail.com
+			// f8ZqkiaR5hwI9QRdc8Dwropue4kENmRp - dmitry.turovtsov@gmail.com
+			// _JsmPE63lCa9UsS45vlKWMlhBhRntoK8 - logikaismekalka@gmail.com
+			// uY9x_ytUQ0sq9-bB8iTvwGnmiWVci4an - web.best.master@gmail.com
+			// RmSQIT1W2KC2_gZf27_KaZ7GWIzpmKJu - ae.fan.game@gmail.com
+			.pipe(gulp.dest('dist'));
+	});
+
 }());
--- other/my-gulp-project/gulpfile.js
+++ other/my-gulp-project/gulpfile.js
@@ -40,15 +40,6 @@
     });
 
 
-
-
-
-
-
-
-
-
-
     gulp.task('default+css', function() { // book
         gulp.run('uglify-js', 'css-base64', 'clean');
     });
@@ -79,8 +70,13 @@
 
     gulp.task('tiny-png', function () {
         gulp.src(['tinypng/**/*.png', 'tinypng/**/*.jpg', 'tinypng/**/*.jpeg'])
-            .pipe(tinypng('uY9x_ytUQ0sq9-bB8iTvwGnmiWVci4an')) // web.best.master
-            //.pipe(tinypng('RmSQIT1W2KC2_gZf27_KaZ7GWIzpmKJu')) // ae.fan.game 234 * 2 = 468
+            .pipe(tinypng('h0DW7VyYVXnl3awj2o7v9wXR-EavOiB5'))
+            // h0DW7VyYVXnl3awj2o7v9wXR-EavOiB5 - kidmathgenius@gmail.com
+            // eSu5nMg0TSDairQWQC_Bx0h41PxKgKEp - mikhail.anisimau.play@gmail.com
+            // f8ZqkiaR5hwI9QRdc8Dwropue4kENmRp - dmitry.turovtsov@gmail.com
+            // _JsmPE63lCa9UsS45vlKWMlhBhRntoK8 - logikaismekalka@gmail.com
+            // uY9x_ytUQ0sq9-bB8iTvwGnmiWVci4an - web.best.master@gmail.com
+            // RmSQIT1W2KC2_gZf27_KaZ7GWIzpmKJu - ae.fan.game@gmail.com
             .pipe(gulp.dest('dist'));
     });
 
