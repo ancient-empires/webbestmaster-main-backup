3d62860d734cff4fe8a6a64f587db9ed83606d3a
webbestmaster <web.best.master@gmail.com>
2015-12-31 23:35:36 +0300

    gl- first attempt

--- tangram-2/www/js/services/device.js
+++ tangram-2/www/js/services/device.js
@@ -16,9 +16,30 @@ var win = window,
 			orientation: ''
 		},
 
+		eventTypes: {
+			down: ['mousedown', 'touchstart'],
+			move: ['mousemove', 'touchmove'],
+			up: ['mouseup', 'touchend']
+		},
+
+		events: {},
+
+		mapEventType: {
+			mousedown: 'down',
+			touchstart: 'down',
+			mousemove: 'move',
+			touchmove: 'move',
+			mouseup: 'up',
+			touchend: 'up'
+		},
+
 		initialize: function () {
 
 			var device = this;
+
+			device.collectInfo();
+
+			device.setPointData({ x: 0, y: 0, scale: 1 });
 
 			device.bindEventListeners();
 
@@ -28,13 +49,270 @@ var win = window,
 
 		},
 
+		collectInfo: function () {
+
+			var device = this,
+				isTouch = 'ontouchstart' in doc,
+				eventTypesIndex = Number(isTouch),
+				types = device.eventTypes,
+				events = device.events;
+
+			// set is touch
+			device.set('isTouch', isTouch);
+
+			// set events names - touch or mouse
+			_.each(types, function (typesArr, key) {
+				events[key] = typesArr[eventTypesIndex];
+			});
+
+		},
+
 		bindEventListeners: function () {
 
-			var device = this;
+			var device = this,
+				events = device.events,
+				body = doc.body;
 
 			win.addEventListener('resize', function () {
 				device.onResize();
 			}, false);
+
+			body.addEventListener(events.down, function (e) {
+				device.onDown(e);
+			}, false);
+
+			body.addEventListener(events.move, function (e) {
+				device.onMove(e);
+			}, false);
+
+			body.addEventListener(events.up, function (e) {
+				device.onUp(e);
+			}, false);
+
+		},
+
+		getEvents: function (e) {
+
+			//e = e.originalEvent; // for jQ like
+
+			var device = this,
+				evt = {
+					events: [],
+					length: 0,
+					type: ''
+				},
+				events = e.touches || [e];
+
+			evt.length = events.length;
+			evt.type = device.mapEventType[e.type];
+
+			_.each(events, function (e) {
+				evt.events.push({
+					x: e.clientX,
+					y: e.clientY
+				});
+			});
+
+			return evt;
+
+		},
+
+		getAverageXY: function (arr) {
+
+			var sumX = 0,
+				sumY = 0,
+				count = arr.length;
+
+			_.each(arr, function (xy) {
+				sumX += xy.x;
+				sumY += xy.y;
+			});
+
+			return {
+				x: sumX / count,
+				y: sumY / count
+			};
+
+		},
+
+		logMoving: function (xy) {
+
+			var device = this,
+				logMoving = device.get('log-moving');
+
+			if (logMoving.length > 10) {
+				logMoving.shift(); // delete first item
+			}
+
+			logMoving.push({
+				x: xy.x,
+				y: xy.y,
+				timeStamp: Date.now()
+			});
+
+			//device.set('log-moving', logMoving);
+
+		},
+
+		clearLogMoving: function () {
+			this.set('log-moving', []);
+		},
+
+		getPinchData: function (events) {
+
+			var device = this,
+				startEvents = device.get('pinchStartEvents'),
+				before,
+				after;
+
+			before = Math.pow(startEvents[0].x - startEvents[1].x, 2) + Math.pow(startEvents[0].y - startEvents[1].y, 2);
+			before = Math.pow(before, 0.5);
+
+			after = Math.pow(events[0].x - events[1].x, 2) + Math.pow(events[0].y - events[1].y, 2);
+			after = Math.pow(after, 0.5);
+
+			return {
+				scale: (after / before) || 1
+			};
+
+		},
+
+		setPointData: function (xys) {
+
+			var device = this;
+
+			if (xys.hasOwnProperty('x')) {
+				device.set('pointDataX', xys.x);
+			}
+
+			if (xys.hasOwnProperty('y')) {
+				device.set('pointDataY', xys.y);
+			}
+
+			if (xys.hasOwnProperty('scale')) {
+				device.set('pointDataScale', xys.scale);
+			}
+
+			return device;
+		},
+
+		getPointData: function () {
+
+			var device = this;
+
+			return {
+				x: device.get('pointDataX'),
+				y: device.get('pointDataY'),
+				scale: device.get('pointDataScale')
+			};
+
+		},
+
+		onDown: function (e) {
+
+			var device = this,
+				events = device.getEvents(e),
+				startEventXY = device.getAverageXY(events.events),
+				pointData = device.getPointData();
+
+			// set start EVENTs position
+			device.set('startDownEventXY', startEventXY);
+
+			// set start point position
+			device.set('startPointData', pointData);
+			device.set('currentPointData', pointData);
+
+			device.set('actionIsActive', true);
+
+			device.clearLogMoving();
+			device.logMoving(startEventXY);
+
+			// detect start zooming
+			if (events.length === 2) {
+				device.set('pinchIsActive', true);
+				device.set('pinchStartEvents', events.events);
+			} else {
+				device.set('pinchIsActive', false);
+			}
+
+		},
+
+		onMove: function (e) {
+
+			if ( !this.get('actionIsActive') ) {
+				return false;
+			}
+
+			var device = this,
+				events = device.getEvents(e),
+				currentEventXY = device.getAverageXY(events.events),
+				currentPointData = device.get('currentPointData'),
+				logMoving = device.get('log-moving'),
+				lastEventXY = logMoving[logMoving.length - 1],
+				pinchData,
+				x,
+				y,
+				dx,
+				dy,
+				scale;
+
+			dx = lastEventXY.x - currentEventXY.x;
+			x = currentPointData.x - dx;
+
+			dy = lastEventXY.y - currentEventXY.y;
+			y = currentPointData.y - dy;
+
+			device.set('currentPointData', {
+				x: x,
+				y: y
+			});
+
+			if ( device.get('pinchIsActive') ) { // zooming
+				pinchData = device.getPinchData(events.events);
+				scale = pinchData.scale;
+				device.setPointData({
+					x: x * scale,
+					y: y * scale,
+					scale: scale
+				});
+			} else { // just moving
+				device.setPointData({
+					x: x,
+					y: y
+				});
+			}
+
+			device.logMoving(currentEventXY);
+
+		},
+
+		onUp: function (e) {
+
+			var device = this,
+				events = device.getEvents(e),
+				eventsArr = events.events,
+				eventsArrLength = eventsArr.length,
+				isTouch = device.get('isTouch');
+
+			if ( !eventsArrLength && isTouch && device.get('pinchIsActive') ) { // 2 fingers -> 0 finger
+				device.set('pinchIsActive', false);
+				device.set('actionIsActive', false);
+				//device.setContainerSize();
+				return;
+			}
+
+			if ( !eventsArrLength || !isTouch) { // if is not touch device - stop moving
+				device.set('actionIsActive', false);
+				//this.sliding();
+				device.clearLogMoving();
+				return;
+			}
+
+			if (eventsArrLength === 1 && isTouch) { // 2 fingers -> 1 finger
+				device.set('pinchIsActive', false);
+				//device.setContainerSize();
+				device.onDown(e);
+			}
 
 		},
 
