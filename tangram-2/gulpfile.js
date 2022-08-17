4e17e9db85489cb2a160dc6876e2036b2c12a268
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2022-08-18 01:41:06 +0800

    refactor: specify css path to process

--- tangram-2/gulpfile.js
+++ tangram-2/gulpfile.js
@@ -16,11 +16,12 @@ const {
 } = require('../common/gulp-helpers');
 
 const paths = {
-  srcGlobs: {
-    html: path.join(__dirname, 'www/**/*.html'),
-    css:  path.join(__dirname, 'www/css/**/*'),
-    js:   path.join(__dirname, 'www/js/**/*')
+  srcDirs: {
+    html: path.join(__dirname, 'www'),
+    css:  path.join(__dirname, 'www/css'),
+    js:   path.join(__dirname, 'www/js'),
   },
+
   destDirs: {
     dist: path.join(__dirname, 'dist'),
 
@@ -29,15 +30,28 @@ const paths = {
     js:   path.join(__dirname, 'dist/www/js'),
   },
 };
-const srcGlobs = paths.srcGlobs;
+const srcDirs = paths.srcDirs;
 const destDirs = paths.destDirs;
+
+paths.srcFilenames = {
+  html: path.join(srcDirs.html, 'index.html'),
+  css:  path.join(srcDirs.css,  'main.css'),
+};
+const srcFilenames = paths.srcFilenames;
+
+paths.srcGlobs = {
+  html: path.join(srcDirs.html, '**/*.html'),
+  css:  path.join(srcDirs.css,  '**/*'),
+  js:   path.join(srcDirs.js,   '**/*'),
+};
+const srcGlobs = paths.srcGlobs;
 
 const basicTasks = {
   copy: {
     /**
      * Copy single file or directory.
-     * @param { string } srcGlob  Source file / directory. (may be written as glob)
-     * @param { string } destDir  Destination directory.
+     * @param { string } srcGlob Source file / directory. (may be written as glob)
+     * @param { string } destDir Destination directory.
      */
     single(srcGlob, destDir) {
       return gulp.src(srcGlob).pipe(gulp.dest(destDir));
@@ -67,12 +81,12 @@ const basicTasks = {
   html: {
     /**
      * Minify HTML.
-     * @param { string } srcGlob  Source file / directory. (may be written as glob)
-     * @param { string } destDir  Destination directory.
-     * @param { minifyHtml.Options } options options for minifying HTML.
+     * @param { string } srcGlob Source file / directory. (may be written as glob)
+     * @param { string } destDir Destination directory.
+     * @param { ?object } options Options for minifying HTML.
      */
     minifyHtml(srcGlob, destDir, options = {}) {
-      return () => {
+      return function minifyHtmlProcess() {
         return gulp.src(srcGlob)
             .pipe(minifyHtml(options))
             .pipe(gulp.dest(destDir))
@@ -81,46 +95,75 @@ const basicTasks = {
   },
 
   css: {
-    importCss() {
-      return gulp.src('./www/css/main.css')
-          .pipe(cssImport({}))
-          .pipe(gulp.dest('./dist/www/css'));
+    /**
+     * Import CSS.
+     * @param { string } srcGlob Source file / directory. (may be written as glob)
+     * @param { string } destDir Destination directory.
+     * @param { ?object } options Options for importing CSS.
+     */
+    importCss(srcGlob, destDir, options = {}) {
+      return function importCssProcess() {
+        return gulp.src(srcGlob)
+            .pipe(cssImport(options))
+            .pipe(gulp.dest(destDir));
+      };
     },
 
-    sass() {
-      return gulp.src('./dist/www/css/main.css')
-          .pipe(sass())
-          .pipe(clean({
-            force: true, // remove original css
-          }))
-          .pipe(gulp.dest('./dist/www/css'));
+    /**
+     * Use SASS to process CSS stylesheets.
+     * @param { string } srcGlob Source file / directory. (may be written as glob)
+     * @param { string } destDir Destination directory.
+     * @param { ?object } options Options for SASS.
+     */
+    sass(srcGlob, destDir, options = {}) {
+      return function sassProcess() {
+        return gulp.src(srcGlob).pipe(sass(options))
+            .pipe(clean({ force: true }))
+            .pipe(gulp.dest(destDir));
+      };
     },
 
-    base64() {
-      return gulp.src('./dist/www/css/main.css')
-          .pipe(cssBase64())
-          .pipe(clean({
-            force: true, // remove original css
-          }))
-          .pipe(gulp.dest('./dist/www/css'));
+    /**
+     * Convert URLs to base64 strings that represent the actual resources.
+     * @param { string } srcGlob Source file / directory. (may be written as glob)
+     * @param { string } destDir Destination directory.
+     * @param { ?object } options Options for base64.
+     */
+    base64(srcGlob, destDir, options = {}) {
+      return function base64Process() {
+        return gulp.src(srcGlob)
+            .pipe(cssBase64(options))
+            .pipe(clean({ force: true }))
+            .pipe(gulp.dest(destDir));
+      };
     },
 
-    autoPrefix() {
-      return gulp.src('./dist/www/css/main.css')
-          .pipe(autoprefixer())
-          .pipe(clean({
-            force: true, // remove original css
-          }))
-          .pipe(gulp.dest('./dist/www/css'));
+    /**
+     * Automatically add vendor prefixes.
+     * @param { string } srcGlob Source file / directory. (may be written as glob)
+     * @param { string } destDir Destination directory.
+     * @param { ?object } options Options for autoprefixer.
+     */
+    autoPrefix(srcGlob, destDir, options = {}) {
+      return function autoPrefixProcess() {
+        return gulp.src(srcGlob).pipe(autoprefixer(options))
+            .pipe(clean({ force: true }))
+            .pipe(gulp.dest(destDir));
+      };
     },
 
-    minifyCss() {
-      return gulp.src('./dist/www/css/main.css')
-          .pipe(minifyCss())
-          .pipe(clean({
-            force: true, // remove original css
-          }))
-          .pipe(gulp.dest('./dist/www/css'));
+    /**
+     * Minify CSS.
+     * @param { string } srcGlob Source file / directory. (may be written as glob)
+     * @param { string } destDir Destination directory.
+     * @param { ?object } options Options for minifying CSS.
+     */
+    minify(srcGlob, destDir, options = {}) {
+      return function minifyCssProcess() {
+        return gulp.src(srcGlob).pipe(minifyCss(options))
+            .pipe(clean({ force: true }))
+            .pipe(gulp.dest(destDir));
+      };
     },
   },
 
@@ -153,24 +196,41 @@ const buildTasks = {
   ),
 
   /**
-     * Minify HTML.
-     * @param { string } srcGlob  Source file / directory. (may be written as glob)
-     * @param { string } destDir  Destination directory.
-     * @param { minifyHtml.Options } options options for minifying HTML.
-     */
+   * Process HTML.
+   * @param { string } srcGlob Source file / directory. (may be written as glob)
+   * @param { string } destDir Destination directory.
+   * @param { ?{ minify: ?object } } options Options for minifying HTML.
+   */
   html(srcGlob, destDir, options = {}) {
     return gulp.series(
       basicTasks.html.minifyHtml(srcGlob, destDir, options),
     );
   },
 
-  css: gulp.series(
-    basicTasks.css.importCss,
-    basicTasks.css.sass,
-    basicTasks.css.base64,
-    basicTasks.css.autoPrefix,
-    basicTasks.css.minifyCss,
-  ),
+  /**
+   * Process CSS.
+   * @param { string } srcMain The CSS file served as the entry point to
+   *   process, typically named "main.css" or "index.css".
+   * @param { string } destDir Destination directory.
+   * @param { ?{
+   *   import: ?object,
+   *   sass: ?object,
+   *   base64: ?object,
+   *   autoPrefix: ?object,
+   *   minify: ?object,
+   * } } options Options for processing CSS.
+   */
+  css(srcMain, destDir, options) {
+    const destMain = path.join(destDir, path.basename(srcFilenames.css));
+
+    return gulp.series(
+      basicTasks.css.importCss(srcMain, destDir, options?.import),
+      basicTasks.css.sass(destMain, destDir, options?.sass),
+      basicTasks.css.base64(destMain, destDir, options?.base64),
+      basicTasks.css.autoPrefix(destMain, destDir, options?.autoPrefix),
+      basicTasks.css.minify(destMain, destDir, options?.minify),
+    );
+  },
 
   js: gulp.series(
     basicTasks.js.es6Import,
@@ -195,8 +255,10 @@ const devTasks = {
     gulp.watch('./www/font/**/*',
         basicTasks.copy.single('./www/font/**/*', path.join(dist, 'font')));
 
-    gulp.watch(srcGlobs.html, buildTasks.html(srcGlobs.html, destDirs.html));
-    gulp.watch('./www/css/**/*', buildTasks.css);
+    gulp.watch(srcGlobs.html,
+        buildTasks.html(srcGlobs.html, destDirs.html));
+    gulp.watch(srcGlobs.css,
+        buildTasks.css(srcGlobs.css, destDirs.css));
     gulp.watch('./www/js/**/*', buildTasks.js);
 
     cb();
@@ -205,8 +267,8 @@ const devTasks = {
 
 module.exports.copy = buildTasks.copy;
 
-module.exports.html = buildTasks.html(paths.srcGlobs.html, paths.destDirs.html);
-module.exports.css = buildTasks.css;
+module.exports.html = buildTasks.html(srcGlobs.html, destDirs.html);
+module.exports.css = buildTasks.css(srcGlobs.css, destDirs.css);
 module.exports.js = buildTasks.js;
 
 // complete build
